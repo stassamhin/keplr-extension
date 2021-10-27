92421667d635f277aa315c2f22c602f0b2fd686f
Stas Samgin <samgin.nitka@gmail.com>
2021-10-27 17:33:34 +0300

    feat: fix metric prefix list

--- packages/unit/src/utils.ts
+++ packages/unit/src/utils.ts
@@ -1,10 +1,6 @@
 type ToMetric = (deep: number) => { remainder: number; prefix: string };
 export const toMetric: ToMetric = (deep) => {
   switch (true) {
-    case deep >= 24:
-      return { remainder: deep - 24, prefix: "yocto" };
-    case deep >= 21:
-      return { remainder: deep - 21, prefix: "zepto" };
     case deep >= 18:
       return { remainder: deep - 18, prefix: "atto" };
     case deep >= 15:
@@ -17,10 +13,6 @@ export const toMetric: ToMetric = (deep) => {
       return { remainder: deep - 6, prefix: "micro" };
     case deep >= 3:
       return { remainder: deep - 3, prefix: "milli" };
-    case deep >= 2:
-      return { remainder: deep - 2, prefix: "centi" };
-    case deep >= 1:
-      return { remainder: deep - 1, prefix: "deci" };
   }
 
   return { remainder: 0, prefix: "" };
